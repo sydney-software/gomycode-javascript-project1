78edf065dba0ef0bbdff286629aa17c980b5dc0f
Sydney Walter <sydneywalter29@gmail.com>
2025-04-22 15:23:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+function isSymmetrical(num) {
+    // Convert the number to a string
+    let strNum = num.toString();
+    
+    // Reverse the string and compare it with the original
+    return strNum === strNum.split("").reverse().join("");
+}
